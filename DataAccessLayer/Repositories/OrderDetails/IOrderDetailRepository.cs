5ce699da57c3f7915dd8ecdee7162d95e46dd96b
anvacuu <anlequoc131@gmail.com>
2026-03-31 16:00:03 +0700

    Quoc An: Add Order models and Repositories

---
+++ DataAccessLayer/Repositories/OrderDetails/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+
+namespace DataAccessLayer.Repositories.OrderDetails
+{
+    public interface IOrderDetailRepository
+    {
+        Task<IEnumerable<OrderDetail>> GetByOrderIdAsync(int orderId);
+        Task AddAsync(OrderDetail detail);
+        void Remove(OrderDetail detail);
+        Task SaveChangesAsync();
+    }
+}
