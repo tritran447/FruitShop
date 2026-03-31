4ab43d1b3f90bb24a45fa4293aead81046bc2bf1
anvacuu <anlequoc131@gmail.com>
2026-03-31 16:22:57 +0700

    Feature: Implement Order and Cart logic (Quoc An)

---
+++ BusinessLogicLayer/Dtos/CartItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Dtos
+{
+    public class CartItemDto
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public int Quantity { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ BusinessLogicLayer/Dtos/CreateOrderDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Dtos
+{
+    public class CreateOrderDetailDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
