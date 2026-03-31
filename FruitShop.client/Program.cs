05d5dac0dd21f9fd266c03ea93c37655753a99f0
anvacuu <anlequoc131@gmail.com>
2026-03-31 16:35:13 +0700

    Fix: Add missing Client project structure and utilities

---
+++ FruitShop.client/Program.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogicLayer.Services.Admin;
+using FruitShop.client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Utilities.Initialize(builder.Configuration);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".FruitShop.Session";
+    options.IdleTimeout = TimeSpan.FromHours(1);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpClient("ApiClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["GivenAPIBaseUrl"]);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
